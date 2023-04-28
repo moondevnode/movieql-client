72364e4eb80cdcc7d4eac02d061279a5300f0aa5
moondevnode <moondevnode@gmail.com>
2023-04-28 22:05:34 +0900

    cache, fragment

--- src/routes/Movie.js
+++ src/routes/Movie.js
@@ -9,6 +9,7 @@ const GET_MOVIE = gql`
             title
             medium_cover_image
             rating
+            isLiked @client
         }
     }
 `;
@@ -54,16 +55,31 @@ const Image = styled.div`
 
 export default function Movie() {
   const { id } = useParams();
-  const { data, loading } = useQuery(GET_MOVIE, {
+  const { data, loading, client: {cache} } = useQuery(GET_MOVIE, {
     variables: {
       movieId: parseInt(id),
     },
   });
+  const onClick = () => {
+    console.log(`clicked Movie:${id}`);
+    cache.writeFragment({
+        id: `Movie:${id}`,
+        fragment: gql`
+            fragment MovieFragment on Movie {
+                isLiked
+            }
+        `,
+        data: {
+            isLiked: !data.movie.isLiked,
+        }
+    })
+  }
   return (
     <Container>
       <Column>
         <Title>{loading ? "Loading..." : `${data.movie?.title}`}</Title>
         <Subtitle>⭐️ {data?.movie?.rating}</Subtitle>
+        <button onClick={onClick}>{data?.movie?.isLiked ? "Unlike": "Like"}</button>
       </Column>
       <Image bg={data?.movie?.medium_cover_image} />
     </Container>
