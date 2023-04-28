a9565109e2326ce50ed6398cae3bda30dc2f8c98
moondevnode <moondevnode@gmail.com>
2023-04-28 18:38:25 +0900

    apolloprovider useState, useEffect

--- src/client.js
+++ src/client.js
@@ -1,23 +1,8 @@
-import {ApolloClient, gql, InMemoryCache} from '@apollo/client';
+import {ApolloClient, InMemoryCache} from '@apollo/client';
 
 const client = new ApolloClient({
     uri: 'http://localhost:4000',
     cache: new InMemoryCache()
 })
-
-client.query({
-    query: gql`
-        {
-            allMovies {
-                id
-                title
-                summary
-                rating
-                url
-                background_image
-            }
-        }
-    `
-}).then(data => console.log(data))
 
 export default client;
